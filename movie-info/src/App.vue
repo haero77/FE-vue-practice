c38ccdb0636cead40081bc3652e07683f0830542
haero77 <shineecard5@gmail.com>
2024-11-24 06:20:59 +0900

    [movie] 영화 목록을 컴포넌트로 만들기

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -2,22 +2,12 @@
 <template>
   <Navbar/>
   <Event :text="text"/>
-  <h1>영화 정보</h1>
-  <div v-for="(movie, movieIndex) in movieData" :key="movieIndex" class="movie-item">
-    <figure>
-      <img :src="`${movie.imgUrl}`" :alt="movie.title">
-    </figure>
-    <div class="movie-info">
-      <h3 class="bg-yellow">{{ movie.title }}</h3>
-      <p>개봉: {{ movie.year }}</p>
-      <p>장르: {{ movie.category }}</p>
-      <button @click="increaseLikeCount(movieIndex)">좋아요</button>
-      <span>{{ movie.like }}</span>
-      <p>
-        <button @click="isModalOpened=true; selectedMovie=movieIndex">상세보기</button>
-      </p>
-    </div>
-  </div>
+  <Movies
+      :movieData="movieData"
+      :isModalOpened="isModalOpened"
+      @openModal="isModalOpened=true; selectedMovie=$event"
+      @increaseLike="increaseLikeCount($event)"
+  />
   <Modal
       :movieData="movieData"
       :isModalOpened="isModalOpened"
@@ -33,6 +23,7 @@ import movieData from "@/assets/movies";
 import Navbar from "@/components/Navbar.vue";
 import Event from "@/components/Event.vue";
 import Modal from './components/Modal.vue';
+import Movies from "@/components/Movies.vue";
 
 export default {
   name: 'App',
@@ -44,16 +35,17 @@ export default {
       text: "props로 보낼 변수값 ~~~ ",
     }
   },
-  methods: {
-    increaseLikeCount(movieIndex) {
-      this.movieData[movieIndex].like++;
-    }
-  },
   // 컴포넌트 등록
   components: {
     Event: Event,
     Navbar: Navbar,
-    Modal: Modal
+    Modal: Modal,
+    Movies: Movies,
+  },
+  methods: {
+    increaseLikeCount(movieIndex) {
+      this.movieData[movieIndex].like++;
+    },
   }
 }
 </script>
---
+++ movie-info/src/components/Movies.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h1>영화 정보</h1>
+    <div v-for="(movie, movieIndex) in movieData" :key="movieIndex" class="movie-item">
+      <figure>
+        <img :src="`${movie.imgUrl}`" :alt="movie.title">
+      </figure>
+      <div class="movie-info">
+        <h3 class="bg-yellow">{{ movie.title }}</h3>
+        <p>개봉: {{ movie.year }}</p>
+        <p>장르: {{ movie.category }}</p>
+        <button @click="$emit('increaseLike', movieIndex)">좋아요</button>
+        <span>{{ movie.like }}</span>
+        <p>
+          <button @click="$emit('openModal', movieIndex)">상세보기</button>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MoviesComponent",
+  props: {
+    movieData: Array,
+    selectedMovie: Number,
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 20px;
+}
+</style>
