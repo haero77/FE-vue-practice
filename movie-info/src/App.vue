f2b374ffba8f0242a18caa22a57112773f931cfd
haero77 <shineecard5@gmail.com>
2024-11-23 23:40:05 +0900

    [movie] 반복문으로 영화 정보 표시하기

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -1,31 +1,44 @@
 <!--  html code -->
 <template>
   <h1>영화 정보</h1>
-  <div>
-    <h3 class="bg-yellow" :style="textRed">{{ title }}</h3>
-    <p>개봉: {{ year }}</p>
-    <p>장르: {{ category }}</p>
+  <div v-for="(movie, i) in movieData" :key="i">
+    <h3 class="bg-yellow">{{ movie.title }}</h3>
+    <p>개봉: {{ movie.year }}</p>
+    <p>장르: {{ movie.category }}</p>
   </div>
 </template>
 
 <!-- js -->
 <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        title: "노량",
-        year: 2023,
-        category: "액션, 드라마",
-        textRed: "color: red",
-      }
+export default {
+  name: 'App',
+  data() {
+    return {
+      movieData: [
+        {
+          title: "트루먼 쇼",
+          year: 1998,
+          category: "드라마",
+        },
+        {
+          title: "해리포터와 마법사의 돌",
+          year: 2001,
+          category: "드라마"
+        },
+        {
+          title: "포레스트 검프",
+          year: 1994,
+          category: "드라마"
+        }
+      ]
     }
   }
+}
 </script>
 
 <!-- css -->
 <style>
-.bg-yellow{
+.bg-yellow {
   background: gold;
 }
 </style>
