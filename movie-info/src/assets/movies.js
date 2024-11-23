147973fa1bb3aae0b5dc0f35a34ef607b7bd8335
haero77 <shineecard5@gmail.com>
2024-11-24 03:28:17 +0900

    [movie] 영화 데이터를 외부에서 가져오기 (default import)

---
+++ movie-info/src/assets/movies.js
@@ -0,0 +1,25 @@
+const movieData = [
+  {
+    title: "트루먼 쇼",
+    category: "드라마",
+    year: 1998,
+    like: 0,
+    imgUrl: require("@/assets/truman_show.jpg"),
+  },
+  {
+    title: "해리포터와 마법사의 돌",
+    category: "드라마",
+    year: 2001,
+    like: 0,
+    imgUrl: require("@/assets/harry_potter_wizard_stone.jpg"),
+  },
+  {
+    title: "포레스트 검프",
+    category: "드라마",
+    year: 1994,
+    like: 0,
+    imgUrl: require("@/assets/forrest_gump.jpg"),
+  }
+];
+
+export default movieData;
