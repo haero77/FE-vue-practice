9f4e46dae41795be6452c5dbe6acafeb3894dd65
haero77 <shineecard5@gmail.com>
2024-11-23 21:22:37 +0900

    [movie] 문서에 데이터 표시하기

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -1,13 +1,31 @@
 <!--  html code -->
 <template>
-  <h1>Hello Vue</h1>
+  <h1>영화 정보</h1>
+  <div>
+    <h3 class="bg-yellow" :style="textRed">{{ title }}</h3>
+    <p>개봉: {{ year }}</p>
+    <p>장르: {{ category }}</p>
+  </div>
 </template>
 
 <!-- js -->
 <script>
-
+  export default {
+    name: 'App',
+    data() {
+      return {
+        title: "노량",
+        year: 2023,
+        category: "액션, 드라마",
+        textRed: "color: red",
+      }
+    }
+  }
 </script>
 
 <!-- css -->
 <style>
+.bg-yellow{
+  background: gold;
+}
 </style>
