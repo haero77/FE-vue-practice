fdd47c0297536c6df790a14d2e3351e7b360e004
haero77 <shineecard5@gmail.com>
2024-11-24 03:45:27 +0900

    [movie] 영화 상세정보 모달창 내용 동적으로 보여주기

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -1,7 +1,7 @@
 "<!--  html code -->
 <template>
   <h1>영화 정보</h1>
-  <div v-for="(movie, i) in movieData" :key="i" class="movie-item">
+  <div v-for="(movie, movieIndex) in movieData" :key="movieIndex" class="movie-item">
     <figure>
       <img :src="`${movie.imgUrl}`" :alt="movie.title">
     </figure>
@@ -9,17 +9,17 @@
       <h3 class="bg-yellow">{{ movie.title }}</h3>
       <p>개봉: {{ movie.year }}</p>
       <p>장르: {{ movie.category }}</p>
-      <button @click="increaseLikeCount(movie)">좋아요</button>
+      <button @click="increaseLikeCount(movieIndex)">좋아요</button>
       <span>{{ movie.like }}</span>
       <p>
-        <button @click="isModalOpened=true">상세보기</button>
+        <button @click="isModalOpened=true; selectedMovie=movieIndex">상세보기</button>
       </p>
     </div>
   </div>
 
   <div class="modal" v-if="isModalOpened">
     <div class="inner">
-      <h3>Detail</h3>
+      <h3>{{ movieData[selectedMovie].title}}</h3>
       <p>영화 상세 정보</p>
       <button @click="isModalOpened=false">닫기</button>
     </div>
@@ -37,11 +37,12 @@ export default {
     return {
       isModalOpened: false,
       movieData: movieData,
+      selectedMovie: 0,
     }
   },
   methods: {
-    increaseLikeCount(movie) {
-      movie.like++;
+    increaseLikeCount(movieIndex) {
+      this.movieData[movieIndex].like++;
     }
   }
 }
