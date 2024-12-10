b923f872810360caa06f0ede556cb84bae792e02
haero77 <shineecard5@gmail.com>
2024-12-10 20:04:33 +0900

    [movie] 검색 기능 구현 (2/2) - 검색된 영화만 노출하기. JS 배열 복사 방법

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -2,9 +2,15 @@
 <template>
   <Navbar/>
   <Event :text="text"/>
-  <SearchBar :movieData="movieData"/>
+  <SearchBar
+      :movieData="movieDataTemp"
+      @searchMovie="searchMovie($event)"
+  />
+  <p>
+    <button class="show-all" @click="showAllMovies">전체보기</button>
+  </p>
   <Movies
-      :movieData="movieData"
+      :movieData="movieDataTemp"
       :isModalOpened="isModalOpened"
       @openModal="isModalOpened=true; selectedMovie=$event"
       @increaseLike="increaseLikeCount($event)"
@@ -33,6 +39,7 @@ export default {
     return {
       isModalOpened: false,
       movieData: movieData,
+      movieDataTemp: [...movieData], // 영화데이터 사본
       selectedMovie: 0,
       text: "props로 보낼 변수값 ~~~ ",
     }
@@ -48,6 +55,15 @@ export default {
   methods: {
     increaseLikeCount(movieIndex) {
       this.movieData[movieIndex].like++;
+    },
+    searchMovie(movieTitle) {
+      // 영화제목이 포함된 데이터를가져옴
+      this.movieDataTemp = this.movieData.filter(movie => {
+        return movie.title.includes(movieTitle);
+      });
+    },
+    showAllMovies() {
+      this.movieDataTemp = [...this.movieData];
     },
   }
 }
