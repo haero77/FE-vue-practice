17283d99ea73ef0659d36b9440c328b39cc4c4e4
haero77 <shineecard5@gmail.com>
2024-12-11 20:45:58 +0900

    [movie] 이벤트창 광고 텍스트 순환 - 컴포넌트 생명주기, mounted, unmounted

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -1,7 +1,7 @@
 "<!--  html code -->
 <template>
   <Navbar/>
-  <Event :text="text"/>
+  <Event :text="text[eventTextNum]"/>
   <SearchBar
       :movieData="movieDataTemp"
       @searchMovie="searchMovie($event)"
@@ -41,7 +41,13 @@ export default {
       movieData: movieData,
       movieDataTemp: [...movieData], // 영화데이터 사본
       selectedMovieId: 0,
-      text: "props로 보낼 변수값 ~~~ ",
+      text: [
+          "트루먼 쇼 사이클",
+          "해리포터와 마법사의 돌 사이클",
+          "포레스트검프 사이",
+      ],
+      eventTextNum: 0,
+      interval: null,
     }
   },
   // 컴포넌트 등록
@@ -69,6 +75,23 @@ export default {
     showAllMovies() {
       this.movieDataTemp = [...this.movieData];
     },
+  },
+  mounted() {
+    console.log("mounted");
+    this.interval = setInterval(() => {
+      // text를 순환
+      this.eventTextNum = (this.eventTextNum + 1) % this.text.length;
+    }, 3000);
+  },
+  unmounted() {
+    /**
+     * 만약 이 컴포넌트가 제거(unmounted)될 때 타이머를 정리하지 않으면:
+     *
+     * 컴포넌트는 사라졌는데도 타이머는 계속 돌아가게 됨
+     * 메모리를 계속 차지하게 됨
+     * 심지어 존재하지 않는 컴포넌트의 eventTextNum을 변경하려고 시도할 수 있음
+     */
+    clearInterval(this.interval);
   }
 }
 </script>
