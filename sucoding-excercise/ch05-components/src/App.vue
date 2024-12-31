338152198471f190eef4bb34b48f2d5548cd39df
haero77 <shineecard5@gmail.com>
2024-12-31 17:05:07 +0900

    [수코딩](ch05 컴포넌트) 5.4.1 컴포넌트의 사용자 정의 속성 - 속성 지정 후 props로 전달 받기

--- sucoding-excercise/ch05-components/src/App.vue
+++ sucoding-excercise/ch05-components/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <FirstChild/>
+
+  <!-- 컴포넌트를 사용할 때 컴포넌트와 함께 속성 정의: '사용자 정의 속성(name, age)'  -->
+  <!-- 정의한 사용자 속성값은 컴포넌트로 전달된다  -->
+  <UserProfile name="김철수" age="30"/>
 </template>
 
 <script>
 // 지역 등록할 컴포넌트 불러오기.
 // -> 지역 등록한 컴포넌트는 컴포넌트 등록한 곳에서만 사용 가능.
 import FirstChild from '@/components/FirstChild.vue';
+import UserProfile from "@/components/UserProfile.vue";
 
 export default {
   components: { // components 속성을 통해 컴포넌트 할당
     FirstChild, // components 속성값은 import 키워드 뒤의 식별자 그대로 사용
+    UserProfile
   },
 }
 </script>
---
+++ sucoding-excercise/ch05-components/src/components/UserProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>이름: {{ name }}</p>
+    <p>나이: {{ age }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "UserProfile",
+  // 속성값 전달 받을 시 props 속성 사용
+  props: ['name', 'age'], // 사용자 정의 속성인 name, age값을 받음
+}
+</script>
+
+
+<style scoped>
+
+</style>
