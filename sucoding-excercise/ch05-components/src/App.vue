3b874a1f49a8561d10893740f506c779567e526f
haero77 <shineecard5@gmail.com>
2024-12-31 16:04:19 +0900

    [수코딩](ch05 컴포넌트) 컴포넌트 지역 등록하기

--- sucoding-excercise/ch05-components/src/App.vue
+++ sucoding-excercise/ch05-components/src/App.vue
@@ -3,7 +3,15 @@
 </template>
 
 <script>
-export default {}
+// 지역 등록할 컴포넌트 불러오기.
+// -> 지역 등록한 컴포넌트는 컴포넌트 등록한 곳에서만 사용 가능.
+import FirstChild from '@/components/FirstChild.vue';
+
+export default {
+  components: { // components 속성을 통해 컴포넌트 할당
+    FirstChild, // components 속성값은 import 키워드 뒤의 식별자 그대로 사용
+  },
+}
 </script>
 
 <style>
--- sucoding-excercise/ch05-components/src/main.js
+++ sucoding-excercise/ch05-components/src/main.js
@@ -4,13 +4,13 @@ import {compile, createApp} from 'vue'
 import App from './App.vue'
 
 // 전역 등록할 컴포넌트 불러오기. 여기서 FirstChild를 '컴포넌트 인스턴스' 라고 함.
-import FirstChild from '@/components/FirstChild.vue';
+// import FirstChild from '@/components/FirstChild.vue';
 // import SecondChild from '@/components/SecondChild.vue';
 
 // 컴포넌트를 전역 등록하려면 애플리케이션 인스턴스 필요.
 // -> 애플리케이션 인스턴스를 변수에 할당
 const app = createApp(App);
-app
-    .component('FirstChild', FirstChild); // component() 애플리케이션 전역 등록
+// app
+//     .component('FirstChild', FirstChild); // component() 애플리케이션 전역 등록
     // .component('SecondChild', SecondChild); // 전역 등록할 컴포넌트 많은 경우
 app.mount('#app');
