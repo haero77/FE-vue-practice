27889c4423bae7cafc23913a764fbbf1159b3020
haero77 <shineecard5@gmail.com>
2025-01-02 05:41:08 +0900

    [수코딩](ch05 컴포넌트) 5.4.2 custom event - 이벤트 검증 'emits' 속성에 발신 가능한 이벤트 명시
    - Vue에서는 공식적으로 emits 기능을 이용해 발신할 이벤트 타입 명시하기를 권장한다.

--- sucoding-excercise/ch05-components/src/App.vue
+++ sucoding-excercise/ch05-components/src/App.vue
@@ -21,7 +21,7 @@ export default {
   },
   data() {
     return {
-      userName: '',
+      userName: '12',
       age: 30
     }
   },
--- sucoding-excercise/ch05-components/src/components/UserProfile.vue
+++ sucoding-excercise/ch05-components/src/components/UserProfile.vue
@@ -13,7 +13,9 @@
 
 <script>
 export default {
-  userName: "UserProfile",
+  // 발신 가능한 부모 컴포넌트의 이벤트 명시
+  emits: ['print-hello'],
+
   // 속성값 전달 받을 시 props 속성 사용
   props: {
     userName: {
@@ -33,7 +35,9 @@ export default {
   methods: {
     // 메서드 핸들러 방식의 이벤트 발신
     parentEventCall() {
-      this.$emit('print-hello'); // 부모 컴포넌트로 @print-hello 이벤트 발신.
+      // emits 옵션 속성으로 print-hello 이벤트만 명시했는데
+      // 다음처럼 명시되지 않은 onPrint-hello 이벤트 호출 시 경고 발생
+      this.$emit('onPrint-hello');
     }
   },
   created() {
