02e4b4e17d59ac190e418c1d08481137e64d1cd3
haero77 <shineecard5@gmail.com>
2025-01-07 06:19:23 +0900

    [수코딩](ch05 컴포넌트) 5.6 슬롯 - 템플릿 조각을 자식 컴포넌트에 전달하고 자식 컴포넌트가 전달받은 템플릿을 렌더링 - 이름이 없는 슬롯

--- sucoding-excercise/ch05-components/src/App.vue
+++ sucoding-excercise/ch05-components/src/App.vue
@@ -1,33 +1,7 @@
 <template>
-  <FirstChild ref="childRef"/>
+  <CustomButton>더블클릭</CustomButton>
 </template>
 
 <script>
-import FirstChild from '@/components/FirstChild.vue';
-
-export default {
-  components: {
-    FirstChild,
-  },
-  mounted() {
-    console.log(this.$refs.childRef.childNum); // 0
-    console.log(this.$refs.childRef.childNumOddAdd); // 짝수
-    console.log(this.$refs.childRef.getChildNum()); // 0
-  }
-}
+import CustomButton from "@/components/CustomButton.vue";
 </script>
-
-<style scoped>
-/*
-  scoped 사용 시 스타일 적용 범위는 현재 컴포넌트로 한정됨.
-  즉 아래의 스타일이 FirstChild 컴포넌트에 적용되지 않음.
- */
-/*
- scoped 사용안 할 경우 부모 컴포넌트 App에서 작성한 스타일은
- 자식 컴포넌트 FirstChild에 영향을 미침.
- */
-
-h1, p {
-  color: orange !important;
-}
-</style>
