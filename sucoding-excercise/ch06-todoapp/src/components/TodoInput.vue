2f089be78930f6b02f1e0f8c81a4b19cc3bc5dca
haero77 <shineecard5@gmail.com>
2025-01-22 21:46:36 +0900

    [수코딩](ch06 todoapp) App 컴포넌트를 기능 별로 분리

---
+++ sucoding-excercise/ch06-todoapp/src/components/TodoInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo__input">
+    <input
+        type="text"
+        class="todo__input-text"
+        placeholder="할 일을 입력하세요."
+    />
+    <button class="todo__input-btn">등록</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader"
+}
+</script>
+
+
+<style scoped>
+
+</style>
