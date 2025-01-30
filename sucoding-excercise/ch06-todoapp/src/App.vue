0a60e61142aeb06db6b48d5f7c080add07a76991
haero77 <shineecard5@gmail.com>
2025-01-30 19:47:12 +0900

    [수코딩](ch06 todoapp 도전과제) 투두 아이템 목록을 localStorage에 저장하여 새로고침 시에도 데이터 유지하도록 변경

--- sucoding-excercise/ch06-todoapp/src/App.vue
+++ sucoding-excercise/ch06-todoapp/src/App.vue
@@ -17,6 +17,7 @@
 import TodoHeader from "@/components/TodoHeader.vue";
 import TodoList from "@/components/TodoList.vue";
 import TodoInput from "@/components/TodoInput.vue";
+import {STORAGE_KEYS} from "@/constants/local-storage-constants.js";
 
 export default {
   components: {
@@ -40,6 +41,25 @@ export default {
       }
     },
   },
+  watch: {
+    todoList: {
+      handler(newTodoList, oldTodoList) {
+        console.log('이전 todoList:', JSON.stringify(oldTodoList));
+        console.log('변경된 todoList:', JSON.stringify(newTodoList));
+
+        // 로컬 스토리지에 데이터 저장
+        localStorage.setItem(STORAGE_KEYS.TODO_LIST, JSON.stringify(newTodoList));
+      },
+      deep: true
+    }
+  },
+  created() {
+    // 로컬 스토리지에서 데이터 가져와서 초기화
+    const savedTodoList = localStorage.getItem(STORAGE_KEYS.TODO_LIST);
+    if (savedTodoList) {
+      this.todoList = JSON.parse(savedTodoList);
+    }
+  },
   methods: {
     updateTab(tab) {
       this.currentTodoStatus = tab;
@@ -59,15 +79,15 @@ export default {
       this.todoList = this.todoList.map(todoItem =>
           // 스프레드 연산자(...)를 사용해 모든 속성을 복사하고
           // completed 속성만 반전시킨 새로운 객체를 만든다.
-          todoItem.id === todoItemIdToToggle ? { ...todoItem, completed: !todoItem.completed } : todoItem
+          todoItem.id === todoItemIdToToggle ? {...todoItem, completed: !todoItem.completed} : todoItem
       )
     },
     deleteTodo(todoItemId) {
       this.todoList = this.todoList.filter(todoItem => todoItem.id !== todoItemId);
     },
-    updateTodoMsg({ id: todoItemId, msg: msgToBe }) {
+    updateTodoMsg({id: todoItemId, msg: msgToBe}) {
       this.todoList = this.todoList.map(todoItem =>
-          todoItem.id === todoItemId ? { ...todoItem, msg: msgToBe } : todoItem
+          todoItem.id === todoItemId ? {...todoItem, msg: msgToBe} : todoItem
       )
     },
   }
