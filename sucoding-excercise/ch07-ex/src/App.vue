2416a46b1b0607ac898b36f6b1a5bbf9ad7d5abf
haero77 <shineecard5@gmail.com>
2025-01-30 21:05:35 +0900

    [수코딩](ch07 Composition API) 7.2.1 반응형 데이터 정의하기: ref는 기본형, reactive는 참조형

--- sucoding-excercise/ch07-ex/src/App.vue
+++ sucoding-excercise/ch07-ex/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-<h1>hello world</h1>
+  <h1>count: {{ count  }}</h1>
+  <h1>state: {{ state }}</h1>
 </template>
 
 <script setup>
+import {ref, reactive} from 'vue'
 
+const count = ref(0);
+const state = reactive({
+  message: 'Hello Vue 3'
+});
 </script>
 
 <style scoped>
